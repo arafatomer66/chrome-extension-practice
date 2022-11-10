53afedd5bf7700af34e3cf785c70376e31adc54b
md omer arafat <arafatomer66@gmail.com>
2022-11-11 00:46:07 +0600

    first commit

---
+++ injector.js
@@ -0,0 +1,4 @@
+document.getElementById('email').value = "testman@thatplace.com";
+document.getElementById('pass').value = "mypassword";
+var chkboxes = document.getElementById("email").value;
+alert(chkboxes);
